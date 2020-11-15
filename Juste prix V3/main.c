1608d54cae65e06ad14ed9be119a656ebabc4bb2
kberz <karlbersia2@gmail.com>
2020-11-15 19:51:58 +0100

    Ajout de trois mode de  difficulté

--- Juste prix V3/main.c
+++ Juste prix V3/main.c
@@ -4,110 +4,132 @@
 
 int main()
 {
-
-    const int MAX = 100, MIN = 1;
-    srand(time(NULL));
-    int nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
     int reponse = 0;
     int compteur = 0;
     int retry = 0;
     int mode_jeu;
+    int difficulter;
+    printf("Vous avez le choix entre trois types de difficultés\n1-Facile : Nombre mystère de 0 à 100\n2-Difficile : Nombre mystère de 0 à 1000\n3-Très difficile : Nombre mystère de 0 à 10 000\n");
+    scanf("%d", &difficulter);
+    int nombreMystere;
 
-    printf("1-Mode solo \n2-Mode duo\n");
-    scanf("%d", &mode_jeu);
+    if (difficulter == 1)
+        {//Déclaration des variables
+        int MAX = 100, MIN = 1;
+        srand(time(NULL));
+        nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+        }
 
-    switch(mode_jeu)
-    {
-    case 1:
-        do
+    if (difficulter == 2)
         {
-            printf("Vous avez bien choisis le mode solo !\nVous pouvez commencer à jouer au juste prix contre l'ordinateur, bonne chance...\n");
-            scanf("%d", &reponse);
-            if (reponse == nombreMystere)
-            printf("Bravo tu as eu la bonne réponse !");
-            else if (reponse < nombreMystere)
-            printf("tu es en dessous de la bonne réponse\n");
-            else if (reponse > nombreMystere)
-            printf("Tu es au dessu de la bonne réponse\n");
+        //Déclaration des variables
+        int MAX = 1000, MIN = 1;
+        srand(time(NULL));
+        nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+        }
 
+    if (difficulter == 3)
+        {//Déclaration des variables
+        int MAX = 10000, MIN = 1;
+        srand(time(NULL));
+        nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+        }
+        //Séléction mode de jeu solo ou duo
+        printf("1-Mode solo \n2-Mode duo\n");
+        scanf("%d", &mode_jeu);
 
-            compteur = compteur + 1;
-            printf("Ton compteur est de %d/8 \n", compteur);
-
-
-            if (compteur == 8)
-            {
-            printf("Fin du biz car tu as perdu trop de fois !\n");
-            printf("Si tu veux recommencer tappe 1:");
-            scanf("%d", &retry);
-            }
-
-
-            if (retry == 1)
-            {
-            printf("Tu veux continuer à souffir c'est ton choix\n");
-            retry --;
-            }
-            else if (retry > 1)
-            {
-            printf("Fin du jeu merci d'avoir jouer !");
-            retry ++;
-            }
-
-            if (compteur >= 8)
-            {
-            compteur = compteur - 8;
-            }
-
-        }while (reponse != nombreMystere || compteur > 8 || retry > 1);
-    break;
-
-    case 2:
-        printf("Vous avez bien choisi le mode duo, Le joueur 1 peux rentrer le nombre mystère !\n");
-        scanf("%d", &nombreMystere);
-        do
+        switch(mode_jeu)
         {
-            printf("Le joueur 2 peux jouer\n");
-            scanf("%d", &reponse);
-            if (reponse == nombreMystere)
-            printf("Bravo tu as eu la bonne réponse !");
-            else if (reponse < nombreMystere)
-            printf("tu es en dessous de la bonne réponse\n");
-            else if (reponse > nombreMystere)
-            printf("Tu es au dessu de la bonne réponse\n");
-
-
-            compteur = compteur + 1;
-            printf("Ton compteur est de %d/8 \n", compteur);
-
-
-            if (compteur == 8)
+        case 1:
+            do //Boucle du mode solo
             {
-            printf("Fin du biz car tu as perdu trop de fois !\n");
-            printf("Si tu veux recommencer tappe 1:");
-            scanf("%d", &retry);
-            }
+                printf("Vous avez bien choisis le mode solo !\nVous pouvez commencer à jouer au juste prix contre l'ordinateur, bonne chance...\n");
+                scanf("%d", &reponse);
+                if (reponse == nombreMystere)
+                printf("Bravo tu as eu la bonne réponse !");
+                else if (reponse < nombreMystere)
+                printf("tu es en dessous de la bonne réponse\n");
+                else if (reponse > nombreMystere)
+                printf("Tu es au dessu de la bonne réponse\n");
 
 
-            if (retry == 1)
+                compteur = compteur + 1;
+                printf("Ton compteur est de %d/8 \n", compteur);
+
+
+                if (compteur == 8)
+                {
+                printf("Fin du biz car tu as perdu trop de fois !\n");
+                printf("Si tu veux recommencer tappe 1:");
+                scanf("%d", &retry);
+                }
+
+
+                if (retry == 1)
+                {
+                printf("Tu veux continuer à souffir c'est ton choix\n");
+                retry --;
+                }
+                else if (retry > 1)
+                {
+                printf("Fin du jeu merci d'avoir jouer !");
+                retry ++;
+                }
+
+                if (compteur >= 8)
+                {
+                compteur = compteur - 8;
+                }
+
+            }while (reponse != nombreMystere || compteur > 8 || retry > 1);
+        break;
+
+        case 2:   //mode de jeu duo
+            printf("Vous avez bien choisi le mode duo, Le joueur 1 peux rentrer le nombre mystère !\n");
+            scanf("%d", &nombreMystere);
+            do
             {
-            printf("Tu veux continuer à souffir c'est ton choix\n");
-            retry --;
-            }
-            else if (retry > 1)
-            {
-            printf("Fin du jeu merci d'avoir jouer !");
-            retry ++;
-            }
+                printf("Le joueur 2 peux jouer\n");
+                scanf("%d", &reponse);
+                if (reponse == nombreMystere)
+                printf("Bravo tu as eu la bonne réponse !");
+                else if (reponse < nombreMystere)
+                printf("tu es en dessous de la bonne réponse\n");
+                else if (reponse > nombreMystere)
+                printf("Tu es au dessu de la bonne réponse\n");
 
-            if (compteur >= 8)
-            {
-            compteur = compteur - 8;
-            }
 
-        }while (reponse != nombreMystere || compteur > 8 || retry > 1);
-    break;
+                compteur = compteur + 1;
+                printf("Ton compteur est de %d/8 \n", compteur);
 
+
+                if (compteur == 8)
+                {
+                printf("Fin du biz car tu as perdu trop de fois !\n");
+                printf("Si tu veux recommencer tappe 1:");
+                scanf("%d", &retry);
+                }
+
+
+                if (retry == 1)
+                {
+                printf("Tu veux continuer à souffir c'est ton choix\n");
+                retry --;
+                }
+                else if (retry > 1)
+                {
+                printf("Fin du jeu merci d'avoir jouer !");
+                retry ++;
+                }
+
+                if (compteur >= 8)
+                {
+                compteur = compteur - 8;
+                }
+
+            }while (reponse != nombreMystere || compteur > 8 || retry > 1);
+        break;
+
+        }
+        return 0;
     }
-    return 0;
-}
